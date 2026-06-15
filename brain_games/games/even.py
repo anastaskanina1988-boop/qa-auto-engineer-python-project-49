ab27869e18f70bc3f924d25fa85d99bae9e0b6b4
Anastasia Kanina <anastaskanina1988@gmail.com>
2026-06-15 12:15:21 +0300

    refactor game modules

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,14 +1,13 @@
-# Сгенерировать случайное число
-# Определить правильный ответ
-# Вернуть вопрос и правильный ответ
-
 import random
 
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
+MIN_NUMBER = 1
+MAX_NUMBER = 100
+
 
 def generate_round():
-    number = random.randint(1, 100)
+    number = random.randint(MIN_NUMBER, MAX_NUMBER)
 
     if number % 2 == 0:
         correct_answer = 'yes'
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,20 +1,5 @@
-import random
-
 from brain_games.engine import run_game
-
-
-DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-
-
-def generate_round():
-    number = random.randint(1, 100)
-
-    if number % 2 == 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-
-    return str(number), correct_answer
+from brain_games.games.even import DESCRIPTION, generate_round
 
 
 def main():
